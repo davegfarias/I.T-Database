0835a77725b56e2860484d4faabc29d44afaec4b
davegfarias <davegfarias@gmail.com>
2023-04-10 01:35:56 -0300

    challenges for Python

---
+++ Cursos/Curso de Python/Desafio #2.py
@@ -0,0 +1,9 @@
+print ('===================================')
+print ('Conhecendo o Dave')
+print ('===================================')
+day_date = input('Em que dia você nasceu?')
+month_date = input('Em que mês você nasceu?')
+year_date = input ('Em que ano você nasceu?')
+
+print('Você nasceu no dia', day_date, 'do mês', month_date, 'do ano de ', year_date)
+print ('Muito prazer em conhecê-lo') 
---
+++ Cursos/Curso de Python/Desafio #3.py
@@ -0,0 +1,8 @@
+print('==================================')
+print('Calculadora Fajuta')
+print('==================================')
+
+number_1 = input('Digite um número')
+number_2 = input ('Digite outro número')
+
+print ('A soma entre', number_1, ' e ', number_2, 'é', number_1 + number_2) 
